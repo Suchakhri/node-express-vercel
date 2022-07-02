8ee2004cc45c7ed7f5582d832d39f9921afa302f
Hxrxn Suchakhri <hxrxn.suchakhri@gmail.com>
2022-07-02 15:57:15 +0700

    Vercel

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.json("Hello World!!!");
 });
 
+app.get("/vercel", (req, res) => {
+  res.json("Hello Vercel");
+});
+
 app.listen(PORT, () => {
   console.log(`Serer is running on PORT : ${PORT}.`);
 });
